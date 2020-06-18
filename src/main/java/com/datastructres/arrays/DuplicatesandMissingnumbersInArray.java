5d762123d04ff3a93d878c3db609fd00ee2dbe5d
RaviKavida <kavida.ravikumar@gmail.com>
2020-06-18 22:18:51 +0530

    array data structres

---
+++ src/main/java/com/datastructres/arrays/DuplicatesandMissingnumbersInArray.java
@@ -0,0 +1,47 @@
+package com.datastructres.arrays;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DuplicatesandMissingnumbersInArray {
+	
+	public static  List<List<Integer>> getmissingAndDuplicate(int arr[]){
+		
+		if(arr.length == 1) {
+			return new ArrayList<>();
+		}
+		int i=0;
+		while(i<arr.length) {
+			if(arr[i] != arr[arr[i]-1]) {
+				int otherIndex = arr[i]-1;
+				int x= arr[i];
+				arr[i] = arr[otherIndex];
+				arr[otherIndex] =x;
+			}else {
+				i++;
+			}
+		}
+		
+		List<Integer> duplicates = new ArrayList<>();
+		List<Integer> missing = new ArrayList<>();
+		
+		for(int j =0;j<arr.length;j++) { 
+			
+			if(arr[j] != j+1) {
+				missing.add(j+1);
+				duplicates.add(arr[j]);
+			}
+		}
+		List<List<Integer>> list = new ArrayList<>();
+		list.add(missing);
+		list.add(duplicates);
+		return list;
+		
+	}
+
+	public static void main(String[] args) {
+		
+	System.out.println(	DuplicatesandMissingnumbersInArray.getmissingAndDuplicate(new int[] {3,4,3,4,3,2}));
+	}
+
+}
---
+++ src/main/java/com/datastructres/arrays/SortAnArray.java
@@ -0,0 +1,42 @@
+package com.datastructres.arrays;
+
+public class SortAnArray {
+
+	public static void main(String[] args) {
+		//with O(n) time complexity, best solution.
+		int arr[]= {3,2,6,5,7,4,1};
+		int i = 0;
+		if(arr.length ==0) {
+			return;
+		}
+		while(i < arr.length) {
+			if(arr[i] != arr[arr[i]-1]) {
+				int otherIndex = arr[i]-1;
+				int x = arr[i];
+				arr[i] = arr[otherIndex];
+				arr[otherIndex] = x;
+			}else {
+			i++;
+			}
+		}
+		for(int k:arr) {
+			System.out.println(k);
+		}
+	System.out.println("------------O(n^2)---");	
+		for(int j=0;i<arr.length;j++) {
+			for(int l=0;l<arr.length;l++) {
+				if(arr[i]<arr[j]) {
+				int temp = arr[i];
+				arr[i] = arr[j];
+				arr[j] =temp;
+				}
+			}
+		}
+		for(int k:arr) {
+			System.out.println(k);
+		}
+		
+		
+	}
+
+}
