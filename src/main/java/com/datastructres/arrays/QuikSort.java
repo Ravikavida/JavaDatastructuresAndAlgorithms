8530b4b3d4a12d2cef855545b57a2d2d6941186b
RaviKavida <kavida.ravikumar@gmail.com>
2020-06-21 00:08:03 +0530

    quick sort implemented

---
+++ src/main/java/com/datastructres/arrays/QuikSort.java
@@ -0,0 +1,45 @@
+package com.datastructres.arrays;
+
+public class QuikSort {
+
+	public static void quicksort(int arr[], int start, int end) {
+		int i;
+		int j;
+		int temp;
+		int pivot;
+		if (start < end) {
+			i = start;
+			pivot = start;
+			j = end;
+
+			while (i < j) {
+				pivot = start;
+				while (arr[i] <= arr[pivot] && i < j)
+					i++;
+				while (arr[j] > arr[pivot])
+					j--;
+				if (i < j) {
+					temp = arr[i];
+					arr[i] = arr[j];
+					arr[j] = temp;
+				}
+			}
+
+			temp = arr[pivot];
+			arr[pivot] = arr[j];
+			arr[j] = temp;
+			quicksort(arr, start, j - 1);
+			quicksort(arr, j + 1, end);
+		}
+	}
+	public static void main(String[] args) {
+		int arr[]= {15,8,2,6,1,18,30,12};
+		QuikSort.quicksort(arr, 0, arr.length-1);
+		
+		for(int k:arr) {
+			System.out.println(k);
+		}
+
+	}
+
+}
