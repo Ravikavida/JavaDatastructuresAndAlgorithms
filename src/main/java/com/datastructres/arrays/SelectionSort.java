d8037c8ab5613e570326c13c5f82b7c318f2936f
RaviKavida <kavida.ravikumar@gmail.com>
2020-06-21 22:03:01 +0530

    bouble sort selectoion sort insertion sort added.

---
+++ src/main/java/com/datastructres/arrays/SelectionSort.java
@@ -0,0 +1,34 @@
+package com.datastructres.arrays;
+
+public class SelectionSort {
+	public static void selectionSort(int arr[]) {
+		if (arr.length <= 1) {
+			return;
+		}
+		for (int i = 0; i < arr.length; i++) {
+			int minindex = i;
+			for (int j = i + 1; j < arr.length; j++) {
+				if ( arr[j] < arr[minindex] ) {
+					minindex = j;
+				}
+			}
+
+			if (minindex != i) {
+				int temp = arr[minindex];
+				arr[minindex] = arr[i];
+				arr[i] = temp;
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+
+		int arr[] = { 4, 1, 6, 5, 0, 2, 1 };
+		SelectionSort.selectionSort(arr);
+
+		for (int k : arr) {
+			System.out.println(k);
+		}
+	}
+
+}
