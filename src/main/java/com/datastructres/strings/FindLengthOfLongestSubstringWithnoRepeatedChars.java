371bada0d01ba57135dedbeabfc30896a2279e80
RaviKavida <kavida.ravikumar@gmail.com>
2020-06-28 12:33:14 +0530

    few algorthms added on strings

---
+++ src/main/java/com/datastructres/strings/FindLengthOfLongestSubstringWithnoRepeatedChars.java
@@ -0,0 +1,38 @@
+package com.datastructres.strings;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FindLengthOfLongestSubstringWithnoRepeatedChars {
+
+	public static int findLongestSubStringLengthWithNorepeatedChars(String s) {
+		if (s == null || s.length() == 0) {
+			return -1;
+		}
+		int max_length = 0;
+		int start = 0;
+		int end = 0;
+		
+		Map<Character,Integer> map = new HashMap<>();
+		
+		while(end < s.length()) {
+			char currentChar = s.charAt(end);
+			if(map.containsKey(currentChar)) {
+				start = Math.max(start,map.get(currentChar)+1);
+			}
+			map.put(currentChar, end);
+			max_length = Math.max(max_length, end-start+1);
+			end++;
+		}
+		return max_length;
+	}
+
+	public static void main(String[] args) {
+		
+		int len = FindLengthOfLongestSubstringWithnoRepeatedChars.findLongestSubStringLengthWithNorepeatedChars("bcdcbababd");
+		
+		System.out.println(len);
+
+	}
+
+}
