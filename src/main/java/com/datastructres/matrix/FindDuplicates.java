3c7fd6a44d3eb7c462171d5ca79343afa90cad2c
RaviKavida <kavida.ravikumar@gmail.com>
2020-07-04 23:09:58 +0530

    finding duplicates in array

--- src/main/java/com/datastructres/matrix/FindDuplicates.java
+++ src/main/java/com/datastructres/matrix/FindDuplicates.java
@@ -1,19 +1,21 @@
 package com.datastructres.matrix;
 
+import java.util.Arrays;
+
 public class FindDuplicates {
 	public static void main(String[] args) {
 
-	int arr[] = { 2, 3, 1, 2, 4, 6, 4, 5,4 };
-	int count[] = new int[arr.length];
-	for(
-	int i = 0;i<arr.length;i++)
-	{
-		if (count[arr[i]] == 1) {
-			System.out.println(arr[i]);
-		} else {
-			count[arr[i]]++;
+		int arr[] = { -1, -1, 1, 2, 3, 4, 4, 3, 6,4};
+		Arrays.sort(arr);
+		for (int i = 0, j = 1; j < arr.length; j++, i++) {
+			if (arr[i] == arr[j]) {
+				System.out.println(arr[i]);
+				while (j < arr.length && arr[i] == arr[j]) {
+					j++;
+				}
+				i = j - 1;
+			}
 		}
+
 	}
 }
-}
-
