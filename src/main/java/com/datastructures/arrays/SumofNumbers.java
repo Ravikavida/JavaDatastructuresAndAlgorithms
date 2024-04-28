ae1731af41d48339bfd83781cf193f83fc9ae32d
kavida.ravi <kavida.ravi@touchngo.com.my>
2024-04-28 21:20:34 +0530

    sum of numbers

---
+++ src/main/java/com/datastructures/arrays/SumofNumbers.java
@@ -0,0 +1,16 @@
+package com.datastructures.arrays;
+
+public class SumofNumbers {
+	
+	public void m1() {
+
+	}
+
+	public static void main(String[] args) {
+		int i = 36;
+		int b = 67;
+		int sum = i + b;
+		System.out.println("sum of num: " + sum);
+	}
+
+}
