87d9a10a99933f74084906fc82aed49ddbca1025
kavida.ravi <kavida.ravi@touchngo.com.my>
2024-05-05 17:33:51 +0530

    adding new element in array & adding number update

--- src/main/java/com/datastructures/arrays/ArrayMinSubArrayMindiffAndElements.java
+++ src/main/java/com/datastructures/arrays/ArrayMinSubArrayMindiffAndElements.java
@@ -3,7 +3,7 @@ package com.datastructures.arrays;
 public class ArrayMinSubArrayMindiffAndElements {
 	
 	public static void main(String[] args) {
-		int arr[] = {3,1,5,7,10,23,15};
+		int arr[] = {3,1,5,7,10,23,15,45};
 		for(int i =0;i<arr.length-1;i++) {
 			if (arr[i] >arr[i+1]) {
 				int nextindex = arr[i];
--- src/main/java/com/datastructures/arrays/SumofNumbers.java
+++ src/main/java/com/datastructures/arrays/SumofNumbers.java
@@ -7,10 +7,11 @@ public class SumofNumbers {
 	}
 
 	public static void main(String[] args) {
-		int i = 96;
+		int i = 986;
 		int b = 67;
 		int sub = i - b;
 		System.out.println("substraction of num: " + sub);
+		System.out.println();
 	}
 
 }
